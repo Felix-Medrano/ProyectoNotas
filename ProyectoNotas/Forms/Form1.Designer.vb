cfc6cc34dd25a355e858c6162d5e242b3f862ad7
Felix-Medrano <sckrool@gmail.com>
2021-03-17 21:05:24 -0600

    Agregado: Menu y btn de Menu. Previsualizacion de titulo(falta titulo)

--- ProyectoNotas/Forms/Form1.Designer.vb
+++ ProyectoNotas/Forms/Form1.Designer.vb
@@ -24,7 +24,21 @@ Partial Class Notas
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Notas))
         Me.imgClip = New System.Windows.Forms.PictureBox()
+        Me.Menu = New System.Windows.Forms.GroupBox()
+        Me.btnEliminar = New System.Windows.Forms.PictureBox()
+        Me.btnOk = New System.Windows.Forms.PictureBox()
+        Me.btnEdit = New System.Windows.Forms.PictureBox()
+        Me.btnCancelar = New System.Windows.Forms.PictureBox()
+        Me.btnAbrirMenu = New System.Windows.Forms.PictureBox()
+        Me.btnCerrarMenu = New System.Windows.Forms.PictureBox()
         CType(Me.imgClip, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Menu.SuspendLayout()
+        CType(Me.btnEliminar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnOk, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnEdit, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnCancelar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnAbrirMenu, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnCerrarMenu, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'imgClip
@@ -37,19 +51,111 @@ Partial Class Notas
         Me.imgClip.TabIndex = 0
         Me.imgClip.TabStop = False
         '
+        'Menu
+        '
+        Me.Menu.Controls.Add(Me.btnEliminar)
+        Me.Menu.Controls.Add(Me.btnOk)
+        Me.Menu.Controls.Add(Me.btnEdit)
+        Me.Menu.Controls.Add(Me.btnCancelar)
+        Me.Menu.Location = New System.Drawing.Point(0, 252)
+        Me.Menu.Name = "Menu"
+        Me.Menu.Size = New System.Drawing.Size(250, 100)
+        Me.Menu.TabIndex = 1
+        Me.Menu.TabStop = False
+        Me.Menu.Text = "Menu"
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Image = CType(resources.GetObject("btnEliminar.Image"), System.Drawing.Image)
+        Me.btnEliminar.Location = New System.Drawing.Point(175, 30)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(50, 50)
+        Me.btnEliminar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.btnEliminar.TabIndex = 4
+        Me.btnEliminar.TabStop = False
+        '
+        'btnOk
+        '
+        Me.btnOk.Image = CType(resources.GetObject("btnOk.Image"), System.Drawing.Image)
+        Me.btnOk.Location = New System.Drawing.Point(25, 30)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(50, 50)
+        Me.btnOk.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.btnOk.TabIndex = 0
+        Me.btnOk.TabStop = False
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Image = CType(resources.GetObject("btnEdit.Image"), System.Drawing.Image)
+        Me.btnEdit.Location = New System.Drawing.Point(100, 30)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(50, 50)
+        Me.btnEdit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.btnEdit.TabIndex = 3
+        Me.btnEdit.TabStop = False
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Image = CType(resources.GetObject("btnCancelar.Image"), System.Drawing.Image)
+        Me.btnCancelar.Location = New System.Drawing.Point(100, 30)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(50, 50)
+        Me.btnCancelar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.btnCancelar.TabIndex = 2
+        Me.btnCancelar.TabStop = False
+        Me.btnCancelar.Visible = False
+        '
+        'btnAbrirMenu
+        '
+        Me.btnAbrirMenu.Image = CType(resources.GetObject("btnAbrirMenu.Image"), System.Drawing.Image)
+        Me.btnAbrirMenu.Location = New System.Drawing.Point(225, 0)
+        Me.btnAbrirMenu.Name = "btnAbrirMenu"
+        Me.btnAbrirMenu.Size = New System.Drawing.Size(25, 25)
+        Me.btnAbrirMenu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.btnAbrirMenu.TabIndex = 5
+        Me.btnAbrirMenu.TabStop = False
+        '
+        'btnCerrarMenu
+        '
+        Me.btnCerrarMenu.Image = CType(resources.GetObject("btnCerrarMenu.Image"), System.Drawing.Image)
+        Me.btnCerrarMenu.Location = New System.Drawing.Point(225, 0)
+        Me.btnCerrarMenu.Name = "btnCerrarMenu"
+        Me.btnCerrarMenu.Size = New System.Drawing.Size(25, 25)
+        Me.btnCerrarMenu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.btnCerrarMenu.TabIndex = 6
+        Me.btnCerrarMenu.TabStop = False
+        Me.btnCerrarMenu.Visible = False
+        '
         'Notas
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(234, 211)
+        Me.ClientSize = New System.Drawing.Size(250, 350)
+        Me.Controls.Add(Me.btnCerrarMenu)
+        Me.Controls.Add(Me.btnAbrirMenu)
+        Me.Controls.Add(Me.Menu)
         Me.Controls.Add(Me.imgClip)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Notas"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         CType(Me.imgClip, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Menu.ResumeLayout(False)
+        CType(Me.btnEliminar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnOk, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnEdit, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnCancelar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnAbrirMenu, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnCerrarMenu, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents imgClip As PictureBox
+    Friend WithEvents Menu As GroupBox
+    Friend WithEvents btnEliminar As PictureBox
+    Friend WithEvents btnOk As PictureBox
+    Friend WithEvents btnEdit As PictureBox
+    Friend WithEvents btnCancelar As PictureBox
+    Friend WithEvents btnAbrirMenu As PictureBox
+    Friend WithEvents btnCerrarMenu As PictureBox
 End Class
--- ProyectoNotas/Forms/Form1.vb
+++ ProyectoNotas/Forms/Form1.vb
@@ -4,6 +4,30 @@
     Dim ultimaX As Integer = 0
     Dim notasMinimizado As Boolean = False
 
+
+
+    Sub _CerrarMenu()
+        btnCerrarMenu.Visible = False
+        btnAbrirMenu.Visible = True
+        Menu.Visible = False
+        If notasMinimizado Then
+            Me.Height = 25
+        Else
+            Me.Height = 250
+        End If
+
+    End Sub
+
+    Sub _AbrirMenu()
+        btnAbrirMenu.Visible = False
+        btnCerrarMenu.Visible = True
+        Menu.Visible = True
+        Me.Height = 350
+    End Sub
+
+
+
+
     Private Sub Notas_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
         General.MouseDown(e)
     End Sub
@@ -30,9 +54,53 @@
             ultimaX = Me.Left
             Me.Size = imgClip.Size
             Me.Left = 0
+            _CerrarMenu()
+            btnAbrirMenu.Visible = False
         Else
             Me.Size = New Point(250, 250)
             Me.Left = ultimaX
+            btnAbrirMenu.Visible = True
         End If
+    End Sub
+
+    Private Sub imgClip_MouseHover(sender As Object, e As EventArgs) Handles imgClip.MouseHover
+        Me.Width = 250
+    End Sub
+
+    Private Sub imgClip_MouseLeave(sender As Object, e As EventArgs) Handles imgClip.MouseLeave
+        If notasMinimizado Then
+            Me.Width = 25
+        End If
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        MsgBox("ok")
+    End Sub
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        btnEdit.Visible = False
+        btnCancelar.Visible = True
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        btnEdit.Visible = True
+        btnCancelar.Visible = False
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        MsgBox("eliminar")
+    End Sub
+
+    Private Sub btnCerrarMenu_Click(sender As Object, e As EventArgs) Handles btnCerrarMenu.Click
+        _CerrarMenu()
+    End Sub
+
+    Private Sub btnAbrirMenu_Click(sender As Object, e As EventArgs) Handles btnAbrirMenu.Click
+        _AbrirMenu()
+    End Sub
+
+    Private Sub Notas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Height = 250
+        Menu.Visible = False
     End Sub
 End Class
